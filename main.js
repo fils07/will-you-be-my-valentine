cd2a9e5d5ba3bebcbdb54be9335778117df0e75f
LN <leonelngoya@gmail.com>
2024-02-10 01:32:56 +0100

    update

--- main.js
+++ main.js
@@ -3,6 +3,64 @@ import gsap from "gsap";
 
 const noButton = document.querySelector(".container-1 .no");
 const yesButton = document.querySelector(".container-1 .yes");
+const yesButton3 = document.querySelector(".container-3 .yes");
+
+yesButton.addEventListener("click", () => {
+  gsap.to(".container-1", {
+    display: "none",
+    duration: 0,
+  });
+  gsap.to(".container-3", {
+    display: "none",
+    duration: 0,
+  });
+  gsap.to(".container-2", {
+    display: "flex",
+    duration: 0,
+  });
+  gsap.to("body", {
+    backgroundColor: "#FF2F6F",
+    duration: 0,
+  });
+});
+
+noButton.addEventListener("click", () => {
+  gsap.to(".container-1", {
+    display: "none",
+    duration: 0,
+  });
+  gsap.to(".container-3", {
+    display: "flex",
+    duration: 0,
+  });
+  gsap.to(".container-2", {
+    display: "none",
+    duration: 0,
+  });
+  gsap.to("body", {
+    backgroundColor: "#09090b",
+    duration: 0,
+  });
+});
+
+yesButton3.addEventListener("click", () => {
+  gsap.to(".container-1", {
+    display: "none",
+    duration: 0,
+  });
+  gsap.to(".container-3", {
+    display: "none",
+    duration: 0,
+  });
+  gsap.to(".container-2", {
+    display: "flex",
+    duration: 0,
+  });
+  gsap.to("body", {
+    backgroundColor: "#FF2F6F",
+    duration: 0,
+  });
+});
 
 let click = 1;
 noButton.addEventListener("mouseenter", () => {
@@ -17,6 +75,14 @@ noButton.addEventListener("mouseenter", () => {
     gsap.to(noButton, {
       bottom: "230px",
       left: "30px",
+      onComplete: () => {
+        gsap.to(noButton, {
+          zIndex: 1,
+        });
+        gsap.to(yesButton, {
+          zIndex: 2,
+        });
+      },
     });
   }
 
@@ -26,10 +92,6 @@ noButton.addEventListener("mouseenter", () => {
       height: "45px",
       bottom: "147px",
       left: "265px",
-      zIndex: 1,
-    });
-    gsap.to(yesButton, {
-      zIndex: 2,
     });
   }
   if (click === 4) {
